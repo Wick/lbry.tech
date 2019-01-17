840b593605db343218196cbc27c02d93f42c0867
Jeremy Kauffman <jeremy@lbry.io>
2019-01-17 16:50:06 -0500

    overview refactor

--- app/components/ecosystem/submodule-wallet.js
+++ app/components/ecosystem/submodule-wallet.js
@@ -1,8 +1,8 @@
 "use strict";
 
+import { require as local } from "app-root-path";
 
-
-//  E X P O R T
+const markdown = local("/app/components/markdown").default;
 
 export default () => `
   <div class="ecosystem__submodule wallet">
@@ -10,16 +10,7 @@ export default () => `
 
     <div class="ecosystem__submodule__description">
       <div class="ecosystem__submodule__markdown">
-        <h4 class="ecosystem__submodule__description__title">Overview</h4>
-        <p>The LBRY app is a graphical browser for the decentralized content marketplace provided by the <a href="https://lbry.io" title="">LBRY</a> protocol. It is essentially the <a href="https://github.com/lbryio/lbry" title="">lbry daemon</a> bundled with an UI using <a href="http://electron.atom.io" title="">Electron</a>.</p>
-
-        <h4 class="ecosystem__submodule__description__title __connection">Connection to...</h4>
-        <p class="__connection-details"></p>
-
-        <h4 class="ecosystem__submodule__description__title">Source</h4>
-        <ul>
-          <li><a href="https://github.com/lbryio/lbry-desktop" title="lbry-desktop source code">https://github.com/lbryio/lbry-desktop</a></li>
-        </ul>
+        ${markdown("./documents/partials/overview/wallet-server.md")}
       </div>
 
       <ul class="__parents">
